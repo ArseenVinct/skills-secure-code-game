7929402a5763971faf19517754dcf0614cc89dd5
Vincent Raes <125125154+ArseenVinct@users.noreply.github.com>
2025-11-26 13:27:05 +0000

    Partial solution for level 4
    Solves the SQL errors. Does not print what the tests want me to print so the level is "incomplete"

--- Season-1/Level-4/code.py
+++ Season-1/Level-4/code.py
@@ -89,24 +89,25 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] get_stock_info\n"
-            query = "SELECT * FROM stocks WHERE symbol = '{0}'".format(stock_symbol)
-            res += "[QUERY] " + query + "\n"
+            query = "SELECT * FROM stocks WHERE symbol = ?"
+            res += "[QUERY] " + query[:-1] + "'" + stock_symbol + "'" + "\n"
 
             # a block list (aka restricted characters) that should not exist in user-supplied input
             restricted_chars = ";%&^!#-"
             # checks if input contains characters from the block list
-            has_restricted_char = any([char in query for char in restricted_chars])
+            has_restricted_char = any([char in stock_symbol for char in restricted_chars])
             # checks if input contains a wrong number of single quotes against SQL injection
-            correct_number_of_single_quotes = query.count("'") == 2
+            # correct_number_of_single_quotes = query.count("'") == 2
 
             # performs the checks for good cyber security and safe software against SQL injection
-            if has_restricted_char or not correct_number_of_single_quotes:
+            # if has_restricted_char or not correct_number_of_single_quotes: No Longer needed
+            if has_restricted_char:
                 # in case you want to sanitize user input, please uncomment the following 2 lines
                 # sanitized_query = query.translate({ord(char):None for char in restricted_chars})
                 # res += "[SANITIZED_QUERY]" + sanitized_query + "\n"
                 res += "CONFIRM THAT THE ABOVE QUERY IS NOT MALICIOUS TO EXECUTE"
             else:
-                cur.execute(query)
+                cur.execute(query, (stock_symbol,))
 
                 query_outcome = cur.fetchall()
                 for result in query_outcome:
@@ -133,13 +134,13 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] get_stock_price\n"
-            query = "SELECT price FROM stocks WHERE symbol = '" + stock_symbol + "'"
-            res += "[QUERY] " + query + "\n"
+            query = "SELECT price FROM stocks WHERE symbol = ?"
+            res += "[QUERY] " + query[:-1] + "'" + stock_symbol + "'" + "\n"
             if ';' in query:
                 res += "[SCRIPT EXECUTION]\n"
-                cur.executescript(query)
+                cur.executescript(query, (stock_symbol,))
             else:
-                cur.execute(query)
+                cur.execute(query, (stock_symbol,))
                 query_outcome = cur.fetchall()
                 for result in query_outcome:
                     res += "[RESULT] " + str(result) + "\n"
